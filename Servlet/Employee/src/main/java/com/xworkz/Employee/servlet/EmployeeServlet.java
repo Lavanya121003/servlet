bf1b96cf42cd580e27a74f85247d27e733262773
Lavanya121003 <lavanyaanand1210@gmail.com>
2024-12-09 23:13:42 +0530

    form

--- Servlet/Employee/src/main/java/com/xworkz/Employee/servlet/EmployeeServlet.java
+++ Servlet/Employee/src/main/java/com/xworkz/Employee/servlet/EmployeeServlet.java
@@ -8,22 +8,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/employee",loadOnStartup = 1)
+@WebServlet(urlPatterns = "/employee", loadOnStartup = 1)
 public class EmployeeServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         System.out.println("Service method..");
 
-        String Name=req.getParameter("ename");
-        String  Email=req.getParameter("eemail");
-        String id=req.getParameter("Id");
-        String Address=req.getParameter("address");
+        String Name = req.getParameter("ename");
+        String Email = req.getParameter("eemail");
+        String id = req.getParameter("Id");
+        String Address = req.getParameter("address");
 
-        System.out.println("Name:"+Name);
-        System.out.println("Email:"+Email);
-        System.out.println("Id:"+id);
-        System.out.println("Address:"+Address);
+        System.out.println("Name:" + Name);
+        System.out.println("Email:" + Email);
+        System.out.println("Id:" + id);
+        System.out.println("Address:" + Address);
 
 
 //        PrintWriter printWriter=resp.getWriter();
@@ -31,18 +31,18 @@ public class EmployeeServlet extends HttpServlet {
 
         resp.setContentType("text/html");
 
-        String htmlResponse="<html"+
-                "<head>"+
-                "<title>Employee Form</title>"+
-                "</head>"+
-                "<body>"+
-                "<p style='color:green;'>"+
-                "Name:"+Name+"<br>"+
-                "Email:<u>"+Email+"</u><br>"+
-                "Id:"+id+"<br>"+
-                "Address:"+Address+
-                "</p>"+
-                "</body>"+
+        String htmlResponse = "<html" +
+                "<head>" +
+                "<title>Employee Form</title>" +
+                "</head>" +
+                "<body>" +
+                "<p style='color:green;'>" +
+                "Name:" + Name + "<br>" +
+                "Email:<u>" + Email + "</u><br>" +
+                "Id:" + id + "<br>" +
+                "Address:" + Address +
+                "</p>" +
+                "</body>" +
                 "</html>";
 
         resp.getWriter().write(htmlResponse);
--- Servlet/Employee/src/main/java/com/xworkz/Employee/servlet/ImageServlet.java
+++ Servlet/Employee/src/main/java/com/xworkz/Employee/servlet/ImageServlet.java
@@ -1,40 +1,6 @@
 package com.xworkz.Employee.servlet;//import javax.servlet.ServletContext;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.io.OutputStream;
-//
-//public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-//
-//    ServletContext cntx= req.getServletContext();
-//    // Get the absolute path of the image
-//    String filename = cntx.getRealPath("Images/button.png");
-//    // retrieve mimeType dynamically
-//    String mime = cntx.getMimeType(filename);
-//    if (mime == null) {
-//        resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-//        return;
-//    }
-//
-//    resp.setContentType(mime);
-//    File file = new File(filename);
-//    resp.setContentLength((int)file.length());
-//
-//    FileInputStream in = new FileInputStream(file);
-//    OutputStream out = resp.getOutputStream();
-//
-//    // Copy the contents of the file to the output stream
-//    byte[] buf = new byte[1024];
-//    int count = 0;
-//    while ((count = in.read(buf)) >= 0) {
-//        out.write(buf, 0, count);
-//    }
-//    out.close();
-//    in.close();
-//
-//}
+
+
 
 
 import javax.servlet.ServletException;
@@ -51,20 +17,20 @@ import java.io.OutputStream;
 public class ImageServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Path to the image file
-        String imagePath = "";
 
-        // Read the image file
+        String imagePath ="C:\\Users\\lavan\\OneDrive\\Pictures\\IMG_20240706_110216_optimized_250.jpg";
+
+
         File imageFile = new File(imagePath);
         FileInputStream fileInputStream = new FileInputStream(imageFile);
         byte[] imageData = new byte[(int) imageFile.length()];
         fileInputStream.read(imageData);
         fileInputStream.close();
 
-        // Set the response content type to image/jpeg
+
         response.setContentType("image/jpeg");
 
-        // Write the image data to the response output stream
+
         OutputStream outputStream = response.getOutputStream();
         outputStream.write(imageData);
         outputStream.close();
